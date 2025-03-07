f9349d2c46c6487060c50f13b78c2006eb7aa9a4
janicedong <1213198891@qq.com>
2025-03-07 23:19:21 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import QRCode from 'qrcode';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const appId = 'wx56b51448c015f42f'
+  const redirectUri = encodeURI('http://vnwiud.natappfree.cc/wechat/callback')
+  const state = 'STATE'
+
+  // const authUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=${state}#wechat_redirect`
+
+  const qrCodeUrl = `https://open.weixin.qq.com/connect/qrconnect?appid=${appId}&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_base&state=${state}#wechat_redirect`
+
+  useEffect(() => {
+    const canvas = document.getElementById('canvas')
+    QRCode.toCanvas(canvas, qrCodeUrl, function (error) {
+      if (error) console.error(error)
+      console.log('success!');
+    })
+  })
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="canvas"></canvas>
     </div>
   );
 }
